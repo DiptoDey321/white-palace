f317538a63841a756931aa15b87f7850fc0a8a06
Dipto Dey <diptodey2005@gmail.com>
2022-10-16 00:28:09 +0600

    enviroment setup completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Compontent/Home/Home';
+import Layout from './Compontent/Layout/Layout';
+import Rooms from './Compontent/Rooms/Rooms';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element : <Layout></Layout>,
+      children: [
+        {
+          path:'/',
+          element : <Home></Home>,
+        },
+        {
+          path:'/rooms',
+          element : <Rooms></Rooms>,
+        },
+        // {
+        //   path:'/topics',
+        //   element : <Topics></Topics>,
+        // },
+        // {
+        //   path:'/statistics',
+        //   loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+        //   element : <Statistics></Statistics>
+        // }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Compontent/Rooms/Rooms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Rooms() {
+  return (
+    <div>
+        <p>
+            Hello i'm from rooms
+        </p>
+    </div>
+  )
+}
+
+export default Rooms
